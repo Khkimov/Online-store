fa80135aaefb332ffcc9b5f4192d08e00cb4efc7
Ilyas <khakim198765@gmail.com>
2022-05-16 23:22:34 +0500

    Add devices

--- server/controllers/brandController.js
+++ server/controllers/brandController.js
@@ -1,11 +1,16 @@
+const { Brand } = require('../models/models');
+
 class BrandController {
   async create(req, res) {
-
+    const { name } = req.body;
+    const brand = Brand.create({ name });
+    return res.json(brand);
   }
 
   async getAll(req, res) {
-
+    const brands = Brand.findAll();
+    return res.json(brands);
   }
 }
 
-module.exports = new BrandController()
+module.exports = new BrandController();
