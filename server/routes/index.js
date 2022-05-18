d73181bcd4600794f8d2b48396ea7d5ef8d4b475
Ilyas <khakim198765@gmail.com>
2022-05-18 21:24:04 +0500

    Done server

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,10 +5,11 @@ const deviceRouter = require('./deviceRouter');
 const userRouter = require('./userRouter');
 const brandRouter = require('./brandRouter');
 const typeRouter = require('./typeRouter');
+const checkRole = require('../middleware/checkRoleMiddleware');
 
 router.use('/user', userRouter);
 router.use('/type', typeRouter);
-router.use('/brand', brandRouter);
-router.use('/device', deviceRouter);
+router.use('/brand', checkRole('ADMIN'), brandRouter);
+router.use('/device', checkRole('ADMIN'), deviceRouter);
 
 module.exports = router;
